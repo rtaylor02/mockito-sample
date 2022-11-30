21b2030b4f94c5d4d8e269cd9562bd5cc3be7eb5
rtaylor02 <taylor.rod.j@gmail.com>
2022-11-30 05:29:23 +0000

    initial commit

---
+++ src/main/java/com/rtaylor02/mockitosample/MockitoSampleApplication.java
@@ -0,0 +1,13 @@
+package com.rtaylor02.mockitosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtaylor02/mockitosample/MockitoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtaylor02.mockitosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
