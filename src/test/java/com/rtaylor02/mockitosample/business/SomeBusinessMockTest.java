cae1adb154e243320ebfcc02c841e9662b0fd468
rtaylor02 <taylor.rod.j@gmail.com>
2022-12-01 05:28:03 +0000

    Refactor SomeBusinessMockTest to use with @InjectMocks and @Mock and @ExtendWith(MockitoExtension.class)

--- src/test/java/com/rtaylor02/mockitosample/business/SomeBusinessMockTest.java
+++ src/test/java/com/rtaylor02/mockitosample/business/SomeBusinessMockTest.java
@@ -3,16 +3,24 @@ package com.rtaylor02.mockitosample.business;
 import com.rtaylor02.mockitosample.data.SomeDataService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
+@ExtendWith(MockitoExtension.class) // Needed to use with @InjectMocks and @Mock
 class SomeBusinessMockTest {
     // Red - Green - Refactor
     // Arrange - Act - Assert
 
+    /* WITHOUT @InjectMocks and @Mock
+    ==================================
     SomeBusinessImpl business = new SomeBusinessImpl();
     SomeDataService serviceMock = mock(SomeDataService.class);
 
@@ -20,6 +28,13 @@ class SomeBusinessMockTest {
     void beforeEachTest() {
         business.setService(serviceMock);
     }
+    ==================================
+     */
+
+    @InjectMocks
+    SomeBusinessImpl business;
+    @Mock
+    SomeDataService serviceMock;
 
     @Test
     void sum_usingSomeDataService() {
