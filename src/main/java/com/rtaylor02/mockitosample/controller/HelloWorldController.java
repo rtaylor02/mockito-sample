1e2bb918a85122f01ca766df56de2e8021b4de86
rtaylor02 <taylor.rod.j@gmail.com>
2022-12-01 23:04:33 +0000

    Created HelloWorldControllerTest.java and helloWorld_basic() in it

--- src/main/java/com/rtaylor02/mockitosample/controller/HelloWorldController.java
+++ src/main/java/com/rtaylor02/mockitosample/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.rtaylor02.mockitosample;
+package com.rtaylor02.mockitosample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/test/java/com/rtaylor02/mockitosample/controller/HelloWorldControllerTest.java
@@ -0,0 +1,38 @@
+package com.rtaylor02.mockitosample.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloWorldController.class)
+class HelloWorldControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    /*
+    We want to test calling GET "/hello-world". We expect the response would be in JSON format.
+    We expect the response would be "Hello world, Rod!!" as content and OK as status.
+     */
+    @Test
+    void helloWorld_basic() throws Exception {
+        // We need to build a request first
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/hello-world")
+                .accept(MediaType.APPLICATION_JSON);
+
+        // Test the response
+        MvcResult response = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World, Rod!!"))
+                .andReturn();
+    }
+}
