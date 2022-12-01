0918704dca175f54eef802ed724bca73b826fdc6
rtaylor02 <taylor.rod.j@gmail.com>
2022-12-01 05:15:55 +0000

    Refactored SomeBusinessMockTest class

--- src/test/java/com/rtaylor02/mockitosample/business/SomeBusinessMockTest.java
+++ src/test/java/com/rtaylor02/mockitosample/business/SomeBusinessMockTest.java
@@ -1,6 +1,7 @@
 package com.rtaylor02.mockitosample.business;
 
 import com.rtaylor02.mockitosample.data.SomeDataService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,46 +10,32 @@ import static org.mockito.Mockito.when;
 
 
 class SomeBusinessMockTest {
+    // Red - Green - Refactor
+    // Arrange - Act - Assert
+
+    SomeBusinessImpl business = new SomeBusinessImpl();
+    SomeDataService serviceMock = mock(SomeDataService.class);
+
+    @BeforeEach
+    void beforeEachTest() {
+        business.setService(serviceMock);
+    }
 
     @Test
     void sum_usingSomeDataService() {
-        SomeBusinessImpl business = new SomeBusinessImpl();
-
-        SomeDataService serviceMock = mock(SomeDataService.class);
         when(serviceMock.retriveAllData()).thenReturn(new int[]{1, 2, 3});
-
-        business.setService(serviceMock);
-
-        int actual = business.sum_usingSomeDataService();
-        int expected = 6;
-        assertEquals(expected, actual);
+        assertEquals(6, business.sum_usingSomeDataService());
     }
 
     @Test
     void sum_empty_usingSomeDataService() {
-        SomeBusinessImpl business = new SomeBusinessImpl();
-
-        SomeDataService serviceMock = mock(SomeDataService.class);
         when(serviceMock.retriveAllData()).thenReturn(new int[]{});
-
-        business.setService(serviceMock);
-
-        int actual = business.sum_usingSomeDataService();
-        int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(0, business.sum_usingSomeDataService());
     }
 
     @Test
     void sum_one_usingSomeDataService() {
-        SomeBusinessImpl business = new SomeBusinessImpl();
-
-        SomeDataService serviceMock = mock(SomeDataService.class);
         when(serviceMock.retriveAllData()).thenReturn(new int[]{3});
-
-        business.setService(serviceMock);
-
-        int actual = business.sum_usingSomeDataService();
-        int expected = 3;
-        assertEquals(expected, actual);
+        assertEquals(3, business.sum_usingSomeDataService());
     }
 }
