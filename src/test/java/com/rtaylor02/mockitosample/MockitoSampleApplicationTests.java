7669346a9ba12eddcd7ed1d7263eb008af743182
rtaylor02 <taylor.rod.j@gmail.com>
2022-12-11 06:56:00 +0000

    Added application.properties in testing environment. Alternatively, use @TestPropertySource
    application.properties overrides the one in main application's path. Non-overridden properties still apply during test. As top priority, @TestPropertySource overrules when used in top level test class.

--- src/test/java/com/rtaylor02/mockitosample/MockitoSampleApplicationTests.java
+++ src/test/java/com/rtaylor02/mockitosample/MockitoSampleApplicationTests.java
@@ -2,8 +2,13 @@ package com.rtaylor02.mockitosample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+/*
+@TestPropertySource: this has the highest priority, i.e. the test application will only use this.
+ */
+//@TestPropertySource(locations = {"classpath:test-configuration.properties"})
 class MockitoSampleApplicationTests {
 
 	@Test
