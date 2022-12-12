b80b06685e5f577aff901f2dca3c6445864a66af
rtaylor02 <taylor.rod.j@gmail.com>
2022-12-12 16:46:55 +0000

    Added HamcrestMatchersTest.java for using Hamcrest matchers library for unit testing

---
+++ src/test/java/com/rtaylor02/mockitosample/learning/HamcrestMatchersTest.java
@@ -0,0 +1,26 @@
+package com.rtaylor02.mockitosample.learning;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class HamcrestMatchersTest {
+    @Test
+    void learning() {
+        List<Integer> numbers = Arrays.asList(12, 15, 45);
+
+        assertThat(numbers, hasSize(3));
+        assertThat(numbers, hasItems(12,45));
+        assertThat(numbers, everyItem(greaterThan(10)));
+        assertThat(numbers, everyItem(lessThan(100)));
+
+        assertThat("", is(emptyString()));
+        assertThat("ABCDE", containsString("BCD"));
+        assertThat("ABCDE", startsWith("ABC"));
+        assertThat("ABCDE", endsWith("CDE"));
+    }
+}
